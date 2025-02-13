c66efefe4f84075c56bdf753acfa50be2d102ce9
k.j.hirner.wisdom@gmail.com <k.j.hirner.wisdom@gmail.com>
2025-02-13 11:10:21 -0800

    updates as of Thu Feb 13 11:10:21 AM PST 2025

---
+++ update.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+Personal\ Access\ Token.py at2cb
+git config --global credential.helper store
+git add .
+#msg=`python -c "m=input('message: '); print(m,end=\" \");"`
+msg="updates as of `date`"
+git commit -am "$msg"
+git push
